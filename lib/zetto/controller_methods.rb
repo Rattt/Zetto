a6f330f2965676bc2eb4c7f9a14696dad24aac53
Ivan <Genom-1990@yandex.ru>
2016-12-09 17:59:04 +0400

    Команда генерации защищенного

--- lib/zetto/controller_methods.rb
+++ lib/zetto/controller_methods.rb
@@ -1,13 +1,19 @@
 module Zetto
   module ControllerMethods
+
     def check_session?(token)
       false
     end
 
     def create_session_for_user?(user)
-      return false unless user.instance_of? Zetto::Config::Params.user_class
+      return false unless user.class == Zetto::Config::Params.user_class
       return false if user.new_record?
-      Zetto::Seance::SessionRegistration.new(user).execute
+      begin
+        Zetto::Session::SessionRegistration.new(user).execute
+      rescue ArgumentError
+        false
+      end
     end
+
   end
 end
