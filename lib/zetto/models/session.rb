5a030dbc374884520147d01d228015ebf67a78f9
Ivan <Genom-1990@yandex.ru>
2016-12-12 13:29:00 +0400

    Добавили побольше алгоритмов

--- lib/zetto/models/session.rb
+++ lib/zetto/models/session.rb
@@ -5,8 +5,8 @@ module Zetto
       belongs_to :user, class_name: Zetto::Config::Params.user_class.to_s, :foreign_key => 'user_id'
 
       # Только алгоритмы из Digest
-      enum algorithm: [ 'MD5', 'SHA1' ]
-
+      enum algorithm: [ 'MD5', 'SHA1', 'RMD160', 'SHA256', 'SHA384', 'SHA512']
+      
       validates :user_id, numericality: { only_integer: true }, presence: true, uniqueness: { case_sensitive: false }
       validates :session_id, presence: true, length: { is: 9 }, uniqueness: { case_sensitive: false }
       validates :algorithm, presence: true
