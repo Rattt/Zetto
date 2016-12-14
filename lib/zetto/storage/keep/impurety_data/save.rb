5ab6c49f875a94e2f10616559ed7e5ef45d08469
Ivan <Genom-1990@yandex.ru>
2016-12-14 16:34:02 +0400

    Сохранение подмешиваемого хеша

--- lib/zetto/storage/keep/impurety_data/save.rb
+++ lib/zetto/storage/keep/impurety_data/save.rb
@@ -7,9 +7,18 @@ module Zetto
 
           def execute(data)
             begin
+              save_data = {}
+              key                        = data['key']
+              save_data['hash_step']     = data['hash_step']
+              save_data['impurity_hash'] = data['impurity_hash']
+
+              if save_data.values.include?(nil) || key == nil
+                raise ArgumentError.new('Incorrect data for storage shunt hash')
+              end
+
               redis = Zetto::Storage::Connect::RedisSingelton.get
-              impurity_hash_key = 'impurity_hash_data:' + data['key'].to_s
-              redis.set(impurity_hash_key, data.to_json)
+              impurity_hash_key = 'impurity_hash_data:' + key.to_s
+              redis.set(impurity_hash_key, save_data.to_json)
               redis.expire(impurity_hash_key, 3600)
             rescue
               puts 'An error occurred Zetto::Storage::ImpuretyData::Save'
