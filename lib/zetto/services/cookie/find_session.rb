1d52d2f9d7575a51c6ea1dfd099dcd4b03dd63f4
Ivan <Genom-1990@yandex.ru>
2016-12-15 14:39:37 +0400

    Уменьшаем количества кода

--- lib/zetto/services/cookie/find_session.rb
+++ lib/zetto/services/cookie/find_session.rb
@@ -1,56 +1,52 @@
-module Zetto
-  module Services
-    module Cookie
+module Zetto::Services::Cookie
 
-      class FindSession
-        include Zetto::Services::Cookie::Modules::Crypto
+  class FindSession
+    include Zetto::Services::Cookie::Modules::Crypto
 
-        def initialize(cookies)
-          unless cookies.class.to_s == "ActionDispatch::Cookies::CookieJar"
-            raise ArgumentError.new('To save session cookies needed, object of ActionDispatch::Cookies::CookieJar')
-          end
-          @cookies = cookies
+    def initialize(cookies)
+      unless cookies.class.to_s == "ActionDispatch::Cookies::CookieJar"
+        raise ArgumentError.new('To save session cookies needed, object of ActionDispatch::Cookies::CookieJar')
+      end
+      @cookies = cookies
+    end
+
+    def execute
+      begin
+        token_data = get_token_from_cookies
+        if token_data.present?
+          get_session_from_db(token_data)
         end
+      rescue
+        puts 'An error occurred Zetto::Services::Cookie::FindSession'
+        nil
+      end
+    end
 
-        def execute
-          begin
-            token_data = get_token_from_cookies
-            if token_data.present?
-              get_session_from_db(token_data)
-            end
-          rescue
-            puts 'An error occurred Zetto::Services::Cookie::FindSession'
-            nil
-          end
-        end
+    private
 
-        private
+    def get_token_from_cookies
+      @cookies[:rembo]
+    end
 
-        def get_token_from_cookies
-          @cookies[:rembo]
-        end
+    def get_session_from_db(token_data)
 
-        def get_session_from_db(token_data)
+      data_session = Zetto::Storage::Keep::Operational::ImpuretyData::Restore.new.execute(token_data)
 
-          data_session = Zetto::Storage::Keep::Operational::ImpuretyData::Restore.new.execute(token_data)
-          
-          data_token = get_data_of_token(data_session['token'], data_session['hash_step'])
-          session = Zetto::Models::Session.find_by(session_id: data_token[:session_id])
-          
-          if secret_hash_correct?(session, data_session['impurity_hash'], data_token[:ciphered_impurity_hash])
-            session
-          end
+      data_token = get_data_of_token(data_session['token'], data_session['hash_step'])
+      session = Zetto::Models::Session.find_by(session_id: data_token[:session_id])
 
-
-        end
-
-        def secret_hash_correct?(sessionObj, impurity_hash, ciphered_impurity_hash)
-          return false if sessionObj.nil?
-          get_ciphered_impurity_hash(sessionObj, impurity_hash) == ciphered_impurity_hash
-        end
-
+      if secret_hash_correct?(session, data_session['impurity_hash'], data_token[:ciphered_impurity_hash])
+        session
       end
 
+
     end
+
+    def secret_hash_correct?(sessionObj, impurity_hash, ciphered_impurity_hash)
+      return false if sessionObj.nil?
+      get_ciphered_impurity_hash(sessionObj, impurity_hash) == ciphered_impurity_hash
+    end
+
   end
+
 end
--- lib/zetto/services/cookie/save_session.rb
+++ lib/zetto/services/cookie/save_session.rb
@@ -1,37 +1,33 @@
-module Zetto
-  module Services
-    module Cookie
+module Zetto::Services::Cookie
 
-      class SaveSession
-        include Zetto::Services::Cookie::Modules::Crypto
+  class SaveSession
+    include Zetto::Services::Cookie::Modules::Crypto
 
-        def initialize(session, cookies)
-          unless session.class == Zetto::Models::Session
-            raise ArgumentError.new('Isn\'t an object of Zetto::Models::Session')
-          end
-          unless cookies.class.to_s == "ActionDispatch::Cookies::CookieJar"
-            raise ArgumentError.new('To save session cookies needed, object of ActionDispatch::Cookies::CookieJar')
-          end
-          @session = session
-          @cookies = cookies
-        end
-
-        def execute
-          begin
-            data = Zetto::Storage::Keep::Operational::ImpuretyData::Generate.new.execute
-            data[:ciphered_impurity_hash] = get_ciphered_impurity_hash(@session, data['impurity_hash'])
-            mixed_hash = get_mix_hashes(@session.session_id, data[:ciphered_impurity_hash], data['hash_step'])
-            @cookies[:rembo] = { data['key'] => mixed_hash}.to_json
-            Zetto::Storage::Keep::Operational::ImpuretyData::Save.new.execute(data)
-            mixed_hash
-          rescue
-            puts 'An error occurred Zetto::Services::Cookie::SaveSession'
-            nil
-          end
-        end
-
+    def initialize(session, cookies)
+      unless session.class == Zetto::Models::Session
+        raise ArgumentError.new('Isn\'t an object of Zetto::Models::Session')
       end
-
+      unless cookies.class.to_s == "ActionDispatch::Cookies::CookieJar"
+        raise ArgumentError.new('To save session cookies needed, object of ActionDispatch::Cookies::CookieJar')
+      end
+      @session = session
+      @cookies = cookies
     end
+
+    def execute
+      begin
+        data = Zetto::Storage::Keep::Operational::ImpuretyData::Generate.new.execute
+        data[:ciphered_impurity_hash] = get_ciphered_impurity_hash(@session, data['impurity_hash'])
+        mixed_hash = get_mix_hashes(@session.session_id, data[:ciphered_impurity_hash], data['hash_step'])
+        @cookies[:rembo] = {data['key'] => mixed_hash}.to_json
+        Zetto::Storage::Keep::Operational::ImpuretyData::Save.new.execute(data)
+        mixed_hash
+      rescue
+        puts 'An error occurred Zetto::Services::Cookie::SaveSession'
+        nil
+      end
+    end
+
   end
+  
 end
