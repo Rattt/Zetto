6023e7e7aeedfcbba1888e5f2f5026da982668e7
Ivan <Genom-1990@yandex.ru>
2016-12-16 17:37:49 +0400

    Востанавливаем тесты

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -4,12 +4,16 @@ describe ApplicationController do
 
   describe "Check of inclusion of methods" do
 
-    it "#create_session_for_user  has to be included" do
-      expect(subject).to respond_to(:create_session_for_user)
+    it "#authentication  has to be included" do
+      expect(subject).to respond_to(:authentication)
     end
 
     it "#current_user  has to be included" do
       expect(subject).to respond_to(:current_user)
+    end
+
+    it "#logout  has to be included" do
+      expect(subject).to respond_to(:logout)
     end
 
   end
