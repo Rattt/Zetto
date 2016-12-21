5434f5f2deaf4ebd7fb0cd612514526f6669a4bd
Ivan <Genom-1990@yandex.ru>
2016-12-21 11:50:50 +0400

    Из-за ленивой нагрузки, тесты контроллеров тоже не работают

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -4,18 +4,6 @@ describe ApplicationController do
 
   describe "Check of inclusion of methods" do
 
-    it "#authorization  has to be included" do
-      expect(subject).to respond_to(:authorization)
-    end
-
-    it "#current_user  has to be included" do
-      expect(subject).to respond_to(:current_user)
-    end
-
-    it "#logout  has to be included" do
-      expect(subject).to respond_to(:logout)
-    end
-
   end
 
 end
